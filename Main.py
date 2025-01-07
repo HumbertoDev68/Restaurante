fc0d23c8ebaabdd99c9a67492fd6ed971fcaf865
Humberto Covilla <humbertopallares237@gmail.com>
2025-01-06 21:00:37 -0500

    Primer Commit. Prueba

---
+++ Main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+# Iniciar tkinter
+app = Tk()
+
+
+# Tamaño de la ventana
+app.geometry('1020x630+0+0')
+
+# Evitar maximizar
+app.resizable(0,0)
+
+# Titulo de la aplicacion
+app.title("Sistema de facturacion")
+
+
+# Evitar que la pantalla se cierre
+app.mainloop()
